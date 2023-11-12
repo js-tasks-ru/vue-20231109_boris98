2355500c1b54d2ca6008504891c2000e190ebece
Boris Zhuravlev <boris.zhuravlev@ixcellerate.ru>
2023-11-12 13:06:57 +0300

    Task 01-02

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,31 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+createApp({
+	data() {
+		return {
+			firstValue: 0,
+			secondValue: 0,
+			resultValue: 0,
+			checkedFunc: "",
+		};
+	},
+	methods: {
+		calcResult() {
+			switch (this.checkedFunc) {
+				case 'sum':
+					this.resultValue=this.firstValue+this.secondValue;
+					break;
+				case 'subtract':
+					this.resultValue=this.firstValue-this.secondValue;
+					break;
+				case 'multiply':
+					this.resultValue=this.firstValue*this.secondValue;
+					break;
+				case 'divide':
+					this.resultValue=this.firstValue/this.secondValue;
+					break;
+			}
+		},
+	},
+}).mount('#app');
