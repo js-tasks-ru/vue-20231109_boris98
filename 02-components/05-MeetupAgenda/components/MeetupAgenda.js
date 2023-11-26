65893933b71b26083f5f5332ba387109140e33ea
Boris Zhuravlev <boris.zhuravlev@ixcellerate.ru>
2023-11-26 12:19:09 +0300

    02-components/ All tasks

--- 02-components/05-MeetupAgenda/components/MeetupAgenda.js
+++ 02-components/05-MeetupAgenda/components/MeetupAgenda.js
@@ -1,13 +1,22 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
-// import MeetupAgendaItem from './MeetupAgendaItem.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
 
 export default defineComponent({
   name: 'MeetupAgenda',
-
+  components: {
+    MeetupAgendaItem,
+  },
+  props: {
+    agenda: {
+      type: Array,
+      required: true,
+    },
+  },
   template: `
     <ul class="agenda">
-      <li class="agenda__item">
+      <li class="agenda__item" v-for="agendaItem in agenda">
         <!-- meetup agenda item -->
+		<MeetupAgendaItem :agendaItem="agendaItem"/>
       </li>
     </ul>`,
 });
