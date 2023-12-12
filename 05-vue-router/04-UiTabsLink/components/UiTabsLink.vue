4a64693ecc17f5ce5e5362fa25ef952f327e17a0
Boris Zhuravlev <boris.zhuravlev@ixcellerate.ru>
2023-12-12 12:15:51 +0300

    05-vue-router / All tasks

--- 05-vue-router/04-UiTabsLink/components/UiTabsLink.vue
+++ 05-vue-router/04-UiTabsLink/components/UiTabsLink.vue
@@ -1,38 +1,46 @@
 <template>
-  <a class="tabs__tab tabs__tab_active" role="tab">Tab</a>
+	<RouterLink :to="to" class="tabs__tab" exactActiveClass="tabs__tab_active" role="tab">
+		<slot />
+	</RouterLink>
 </template>
 
 <script>
 export default {
-  name: 'UiTabsLink',
+	name: 'UiTabsLink',
+	props: {
+		to: {
+			type: [Object, String],
+			required: true,
+		},
+	},
 };
 </script>
 
 <style scoped>
 /* _tabs.css */
 .tabs__tab {
-  display: inline-flex;
-  padding: 10px 0;
-  font-weight: 400;
-  font-size: 18px;
-  text-decoration: none;
-  line-height: 28px;
-  color: var(--grey-8);
-  border-bottom: 2px solid transparent;
-  cursor: pointer;
-  margin-right: 18px;
-  transition-duration: 0.2s;
-  transition-property: border-color, color;
-  box-shadow: none;
-  background-color: transparent;
-  outline: none;
-  position: relative;
-  z-index: 1;
+	display: inline-flex;
+	padding: 10px 0;
+	font-weight: 400;
+	font-size: 18px;
+	text-decoration: none;
+	line-height: 28px;
+	color: var(--grey-8);
+	border-bottom: 2px solid transparent;
+	cursor: pointer;
+	margin-right: 18px;
+	transition-duration: 0.2s;
+	transition-property: border-color, color;
+	box-shadow: none;
+	background-color: transparent;
+	outline: none;
+	position: relative;
+	z-index: 1;
 }
 
 .tabs__tab:hover,
 .tabs__tab.tabs__tab_active {
-  border-bottom-color: var(--blue);
-  color: var(--blue);
+	border-bottom-color: var(--blue);
+	color: var(--blue);
 }
 </style>
