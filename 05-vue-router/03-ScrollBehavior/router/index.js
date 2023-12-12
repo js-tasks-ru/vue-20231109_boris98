270102f712d6ceba952d2e5abaa16081eb3f2bb3
Boris Zhuravlev <boris.zhuravlev@ixcellerate.ru>
2023-12-12 11:58:08 +0300

    05-vue-router / All tasks

--- 05-vue-router/03-ScrollBehavior/router/index.js
+++ 05-vue-router/03-ScrollBehavior/router/index.js
@@ -41,4 +41,11 @@ export const router = createRouter({
       ],
     },
   ],
+  scrollBehavior(to, from, savedPosition) {
+    if (to.meta.saveScrollPosition && from.meta.saveScrollPosition) return false;
+    if (savedPosition) return savedPosition;
+    if (to.hash) return { el: to.hash };
+
+    return { left: 0, top: 0 };
+  },
 });
