d36444e0430f0e61ff4fbbd9baa940548e76949c
Boris Zhuravlev <boris.zhuravlev@ixcellerate.ru>
2023-12-17 21:08:08 +0300

    06-wrappers / All tasks

--- 06-wrappers/05-UiImageUploader/components/UiImageUploader.vue
+++ 06-wrappers/05-UiImageUploader/components/UiImageUploader.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="image-uploader">
-    <label class="image-uploader__preview image-uploader__preview-loading" style="--bg-url: url('/link.jpeg')">
-      <span class="image-uploader__text">Загрузить изображение</span>
-      <input type="file" accept="image/*" class="image-uploader__input" />
+    <label class="image-uploader__preview" :class="{ 'image-uploader__preview-loading': state === 'loading' }">
+      <span class="image-uploader__text">{{ uploaderText }}</span>
+      <input
+        type="file"
+        ref="input"
+        accept="image/*"
+        class="image-uploader__input"
+        v-bind="$attrs"
+        @click="handleRemove"
+        @change="handleChange"
+      />
     </label>
   </div>
 </template>
@@ -10,6 +18,84 @@
 <script>
 export default {
   name: 'UiImageUploader',
+  inheritAttrs: false,
+
+  props: {
+    preview: String,
+    uploader: Function,
+  },
+  created() {
+    if (this.preview) this.state = 'filled';
+  },
+
+  emits: ['remove', 'select', 'error', 'upload'],
+
+  data() {
+    return {
+      state: 'empty',
+      previewURL: this.preview,
+      file: null,
+    };
+  },
+  computed: {
+    backgroundImage() {
+      return this.previewURL ? `url(${this.previewURL})` : 'var(--default-cover)';
+    },
+    uploaderText() {
+      if (this.state === 'loading') return 'Загрузка...';
+      if (this.state === 'filled') return 'Удалить изображение';
+      return 'Загрузить изображение';
+    },
+  },
+  methods: {
+    handleChange(event) {
+      this.file = event.target.files[0];
+      if (this.previewURL && typeof this.previewURL === 'object') {
+        URL.revokeObjectURL(this.selectedURL);
+      }
+      if (this.file) {
+        this.previewURL = URL.createObjectURL(this.file);
+      } else {
+        this.previewURL = '';
+      }
+      this.$emit('select', this.file);
+      this.uploadFile(this.file);
+    },
+    async uploadFile(file) {
+      if (this.uploader) {
+        this.state = 'loading';
+        try {
+          const result = await this.uploader(file);
+          this.handleSuccessUpload(result);
+        } catch (error) {
+          this.handleError(error);
+        }
+      } else {
+        this.state = 'filled';
+      }
+    },
+    handleSuccessUpload(result) {
+      this.state = 'filled';
+      this.$emit('upload', result);
+    },
+
+    handleError(error) {
+      this.$emit('error', error);
+      this.resetUploader();
+    },
+    resetUploader() {
+      this.previewURL = null;
+      this.$refs.input.value = null;
+      this.state = 'empty';
+    },
+    handleRemove(event) {
+      if (this.state === 'filled') {
+        event.preventDefault();
+        this.$emit('remove');
+        this.resetUploader();
+      }
+    },
+  },
 };
 </script>
 
@@ -26,7 +112,7 @@ export default {
   --bg-url: var(--default-cover);
   background-size: cover;
   background-position: center;
-  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(--bg-url);
+  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), v-bind(backgroundImage);
   border: 2px solid var(--blue-light);
   border-radius: 8px;
   transition: 0.2s border-color;
