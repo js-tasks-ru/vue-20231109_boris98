8bce3978f0d0009e31ac0e4937271de3c4f3a0e7
Boris Zhuravlev <boris.zhuravlev@ixcellerate.ru>
2023-12-21 10:49:19 +0300

    07-forms / All tasks

--- 07-forms/01-CounterButtonComplete/components/CounterButton.vue
+++ 07-forms/01-CounterButtonComplete/components/CounterButton.vue
@@ -1,9 +1,41 @@
 <template>
-  <button></button>
+  <button @click="handleClick">
+    {{ localCount }}
+  </button>
 </template>
 
 <script>
 export default {
   name: 'CounterButton',
+
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  emits: ['update:count'],
+
+  data() {
+    return {
+      localCount: null,
+    };
+  },
+
+  watch: {
+    count: {
+      immediate: true,
+      handler(newValue) {
+        this.localCount = newValue;
+      },
+    },
+  },
+  methods: {
+    handleClick() {
+      this.localCount++;
+      this.$emit('update:count', this.localCount);
+    },
+  },
 };
 </script>
