d87b0e6701dc1dad3e5f11050fe03c12dc8842f9
Boris Zhuravlev <boris.zhuravlev@ixcellerate.ru>
2023-12-29 17:17:36 +0300

    08-reactivity / All tasks

--- 08-reactivity/05-reactify/utils/reactify.js
+++ 08-reactivity/05-reactify/utils/reactify.js
@@ -1,4 +1,4 @@
-import { computed } from 'vue';
+import { computed, unref } from 'vue';
 
 /**
  * @template T
@@ -6,11 +6,12 @@ import { computed } from 'vue';
  * @returns {function(...[*]): ComputedRef<T>} - Функция вычисления от ref-ов, возвращающая вычисляемое значение computed
  */
 export function reactify(func) {
-  // ...
-  // return () => computed(() => {});
-  return (...args) =>
-  computed(() => {
-	const values = args.map((arg) => (arg.value === undefined ? arg : arg.value));
-	return func(...values);
-  });
+	// ...
+	// return () => computed(() => {});
+	return (...args) =>
+    computed(() => {
+      const values = args.map((arg) => unref(arg));
+
+      return func(...values);
+    });
 }
